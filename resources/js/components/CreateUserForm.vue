bd607dcbdc3867b955902931525fcce6871d1a84
muhammed-mohsen <41347166+muhammed-mohsen@users.noreply.github.com>
2021-03-27 14:52:33 +0200

    fix prev button in create User form

--- resources/js/components/CreateUserForm.vue
+++ resources/js/components/CreateUserForm.vue
@@ -137,9 +137,9 @@
                 </div>
         </div>
        </div> 
-        <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-between">
 
-          <button class="btn btn-secondry" @click.prevent="$emit('prevClicked')" >
+          <button class="btn btn-secondary" @click.prevent="$emit('prevClicked')" >
                 previous
             </button>
           <button class="btn btn-primary" @click.prevent="createUser" >
