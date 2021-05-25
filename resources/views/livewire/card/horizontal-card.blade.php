4239b6ad8c6cdf2368a7061563151ef2094881b3
amgadamin <78267266+amgadamin@users.noreply.github.com>
2021-05-26 00:57:29 +0300

    correct currency code
    correct currency code
    Remove short description البحر

--- resources/views/livewire/card/horizontal-card.blade.php
+++ resources/views/livewire/card/horizontal-card.blade.php
@@ -2,8 +2,8 @@
     <a href="#">
         <div class="photo"><img src="{{$product->getLastMediaUrl('product_variation-images') ?: asset('/assets/website/images/slider/img-1.jpg')}}" alt=""></div>
         <div class="content">
-            <h2 class="title">{{$product->name}}<br/>البحر</h2>
-            <p class="price">{{$product->price->amount()}} ريال </p>
+            <h2 class="title">{{$product->name}}<br/></h2>
+            <p class="price">{{$product->price->amount()}} {{__('website.riyals')}} </p>
         </div></a>
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
 </div>
